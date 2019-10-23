2ffd045e68241467f486db6d960676b86d3cb4da
Jan Vlnas <git@jan.vlnas.cz>
2019-10-23 23:47:27 +0200

    Clean-up defaults

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-console.log('imports done');
-
 const {PORT, NODE_ENV} = process.env;
 const dev = NODE_ENV === 'development';
 
