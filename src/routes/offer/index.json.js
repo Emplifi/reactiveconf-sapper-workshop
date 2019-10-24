fb2e4d42da37c337c9163af0bc397cc93dd36c1f
Jan Vlnas <git@jan.vlnas.cz>
2019-10-24 11:05:34 +0200

    Task: Implement Offer page

--- src/routes/offer/index.json.js
+++ src/routes/offer/index.json.js
@@ -2,48 +2,31 @@ import send from '@polka/send';
 import {getProducts} from '../../cms';
 
 export async function get(req, res) {
-  // const productsResponse = await getProducts();
-  const itemsByCategory = {
-    Desserts: [
-      {
-        name: 'Double Chocolate Cupcakes ',
-        image: {
-          dimensions: {
-            width: 640,
-            height: 640,
-          },
-          alt: null,
-          copyright: null,
-          url:
-            'https://sapperweb.cdn.prismic.io/sapperweb/bfc8a1d91fce6c4c7152c97069830fcb8f5f80b5_chocolate-cupcakes-2-400x400.jpg',
-          Snipcart: {
-            dimensions: {
-              width: 50,
-              height: 50,
-            },
-            alt: null,
-            copyright: null,
-            url:
-              'https://sapperweb.cdn.prismic.io/sapperweb/d03b32034a0ce6bbab9063128c4ca3de01cb1ded_chocolate-cupcakes-2-400x400.jpg',
-          },
-          Listing: {
-            dimensions: {
-              width: 300,
-              height: 300,
-            },
-            alt: null,
-            copyright: null,
-            url:
-              'https://sapperweb.cdn.prismic.io/sapperweb/fb54d5adc8feb5578cb9e84f95d41008aed8784e_chocolate-cupcakes-2-400x400.jpg',
-          },
-        },
-        price: 7,
-        description: 'Chocolate, eggs, vanilla, milk',
-      },
-    ],
-  };
+  const productsResponse = await getProducts();
+  const itemsByCategory = productsResponse.results.reduce(reduceCategories, {});
   send(res, 200, JSON.stringify(itemsByCategory), {
     'Cache-Control': `max-age=0, s-max-age=${600}`, // 10 minutes
     'Content-Type': 'application/json',
   });
+}
+
+function reduceCategories(list, {data}) {
+  if (!data.categories[0].link.data) {
+    console.log('malformed', data);
+    return list;
+  }
+  const {categories, title, image, price, description} = data;
+  const categoryName = categories[0].link.data.title[0].text;
+  const mealName = title[0].text;
+  const descriptionText = description[0].text;
+
+  return {
+    ...list,
+    [categoryName]: (list[categoryName] || []).concat({
+      name: mealName,
+      image,
+      price,
+      description: descriptionText,
+    }),
+  };
 }
