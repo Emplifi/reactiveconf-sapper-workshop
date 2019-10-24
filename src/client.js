f3b7553a806700d7c0be07ce985512d8c5763ed6
Jan Vlnas <git@jan.vlnas.cz>
2019-10-24 11:09:34 +0200

    Bonus: Add Analytics

--- src/client.js
+++ src/client.js
@@ -1,5 +1,10 @@
 import * as sapper from '@sapper/app';
+import {setupAnalytics} from './analytics';
 
 sapper.start({
   target: document.querySelector('#sapper'),
 });
+
+if (process.env.TRACKING_ID) {
+  setupAnalytics(process.env.TRACKING_ID);
+}
