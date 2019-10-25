5beb50d2504e22df57c37c2698a3c340bcb44942
Adam Sobotka <adam.sobotka@socialbakers..com>
2019-10-25 16:14:12 +0200

    killu

--- src/routes/offer/index.json.js
+++ src/routes/offer/index.json.js
@@ -2,7 +2,7 @@ import send from '@polka/send';
 //import {getProducts} from '../../cms';
 
 export async function get(req, res) {
-  const productsResponse = await fetch("/lambda");
+  const productsResponse = await fetch("../../lambda");
   const itemsByCategory = productsResponse.results.reduce(reduceCategories, {});
   send(res, 200, JSON.stringify(itemsByCategory), {
     'Cache-Control': `max-age=0, s-max-age=${600}`, // 10 minutes
